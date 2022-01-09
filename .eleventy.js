e753c0ec97537f3c77f67743a8f88eaf6b753701
Oliver Xie <oliverxie19@gmail.com>
2022-01-09 12:44:36 -0800

    move static resources to own folder

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,5 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("images");
-    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("static");
 
     return {
         passthroughFileCopy: true
